3e513c4a971039612ef4ec60fa0c2e30314d7b1e
Marcel Naef <me@mauricenaef.ch>
2019-03-25 07:37:40 +0100

    Push 1.0.4

--- dist/themes/dravet.2019/header.php
+++ dist/themes/dravet.2019/header.php
@@ -21,6 +21,15 @@
 	<meta name="Copyright" content="Copyright <?php echo date('Y') ?>, alle Rechte vorbehalten | Konzept, Design und Umsetzung von MAURICE NAEF webdesign, https://mauricenaef.ch" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+	<!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com -->
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+	<link rel="preload" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600i,800" as="fetch" crossorigin="anonymous">
+	<script type="text/javascript">
+	!function(e,n,t){"use strict";var o="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600i,800",r="__3perf_googleFontsStylesheet";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage);
+	</script>
+	<!-- End of code snippet for Google Fonts -->
+
 </head>
 
 <body <?php body_class('site'); ?>>
