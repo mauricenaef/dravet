154b004917a7e07a9be47a71b50b3fedea451cde
Marcel Naef <me@mauricenaef.ch>
2019-04-10 15:17:40 +0200

    update login

--- src/theme/inc/admin-carbon-fields.php
+++ src/theme/inc/admin-carbon-fields.php
@@ -2,25 +2,117 @@
 
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
+use Carbon_Fields\Block;
 
 // Google Maps API
 add_filter( 'carbon_fields_map_field_api_key', 'crb_get_gmaps_api_key' );
 function crb_get_gmaps_api_key( $current_key ) {
-    return 'api';
+    return 'AIzaSyA00G0NzDtOzdQRcQc6QRdo5u7lyj7Yz7I';
 }
 
 // Front Page Fields
 add_action( 'carbon_fields_register_fields', 'custom_carbon_fields_front_page' );
 function custom_carbon_fields_front_page() {
-    Container::make( 'post_meta', 'Seiten Meta' )
-    ->where( 'post_id', '=', get_option( 'page_on_front' ) )
+    
+    Container::make( 'post_meta', 'Event Infos' )
+    //->set_context( 'side' )
+    ->where( 'post_type', '=', 'post' )
     ->add_fields( array(
-        Field::make( 'text', 'home_video_url', 'Intro Video URL' ),
-        Field::make( 'image', 'home_intro_icon', 'Bild / Icon' ),
-        Field::make( 'media_gallery', 'home_gallery', 'Bilder Galerie' )
+        Field::make( 'date_time', 'crb_event_start_date', __( 'Event Datum' ) ),
+        Field::make( 'map', 'crb_company_location', __( 'Location' ) )
+        ->set_help_text( __( 'drag and drop the pin on the map to select location' ) )
+        ->set_position( 47.376888, 8.541694, 12 ),
+        Field::make( 'media_gallery', 'gallery', 'Bilder Galerie' )
         ->set_type( array( 'image' ) )
         ->set_duplicates_allowed( false ),
+        
     ));
+
+    
+    $card_labels = array(
+        'plural_name' => 'Cards',
+        'singular_name' => 'Card',
+    );
+
+    $faq_labels = array(
+        'plural_name' => 'FAQs',
+        'singular_name' => 'FAQ',
+    );
+
+    Block::make( __( 'Card Element' ) )
+    ->add_fields( array(
+        Field::make( 'complex', 'crb_slider', __( 'Card Element' ) )
+        ->setup_labels( $card_labels )
+        ->add_fields( array(
+            Field::make( 'text', 'title', __( 'Element Titel' ) ),
+            Field::make( 'text', 'subtitle', __( 'Element Subtitel Titel (optional)' ) ),
+            Field::make( 'image', 'photo', __( 'Element Bild / Logo' ) ),
+            Field::make( 'rich_text', 'content', __( 'Inhalt' ) ),
+        ) )
+        ->set_header_template( '
+        <% if (title) { %>
+            <%- title %> <%- title ? "" : "" %>
+        <% } %>
+    ' )
+    ) )
+    ->set_icon( 'schedule' )
+    ->set_category( 'dravet-category', __( 'Dravet Blöcke' ) )
+    ->set_preview_mode( false )
+    //->set_description( __( 'A simple block consisting of a heading, an image and a text content.' ) )
+    ->set_render_callback( function ( $block ) {
+        ?>
+
+        <div class="block">
+            <div class="block__heading">
+                <h1><?php echo esc_html( $block['title'] ); ?></h1>
+            </div><!-- /.block__heading -->
+
+            <div class="block__image">
+                <?php echo wp_get_attachment_image( $block['photo'], 'full' ); ?>
+            </div><!-- /.block__image -->
+
+            <div class="block__content">
+                <?php echo apply_filters( 'the_content', $block['content'] ); ?>
+            </div><!-- /.block__content -->
+        </div><!-- /.block -->
+
+        <?php
+    } );
+
+
+    Block::make( __( 'FAQ Element' ) )
+    ->add_fields( array(
+        Field::make( 'complex', 'crb_slider', __( 'FAQ Element' ) )
+        ->setup_labels( $faq_labels )
+        ->add_fields( array(
+            Field::make( 'text', 'title', __( 'Frage' ) ),
+            Field::make( 'rich_text', 'content', __( 'Antwort' ) ),
+        ) )
+        ->set_header_template( '
+        <% if (title) { %>
+            <%- title %> <%- title ? "" : "" %>
+        <% } %>
+    ' )
+    ) )
+    ->set_icon( 'testimonial' )
+    ->set_category( 'dravet-category', __( 'Dravet Blöcke' ) )
+    ->set_preview_mode( false )
+    //->set_description( __( 'A simple block consisting of a heading, an image and a text content.' ) )
+    ->set_render_callback( function ( $block ) {
+        ?>
+
+        <div class="block">
+            <div class="block__heading">
+                <h1><?php echo esc_html( $block['title'] ); ?></h1>
+            </div><!-- /.block__heading -->
+
+            <div class="block__content">
+                <?php echo apply_filters( 'the_content', $block['content'] ); ?>
+            </div><!-- /.block__content -->
+        </div><!-- /.block -->
+
+        <?php
+    } );
 }
 
 // Rearange Angebote Columns
