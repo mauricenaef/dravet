328cfb368edf1208291d09a7ddccf1171dcd8e9f
Marcel Naef <me@mauricenaef.ch>
2019-10-21 22:20:34 +0200

    fix show menu location

--- src/theme/functions/navigation.php
+++ src/theme/functions/navigation.php
@@ -4,23 +4,20 @@
  *
  * @package dravet
  */
+// This theme uses wp_nav_menu() in one location.	
+register_nav_menus( 
+	array(
+		'primary' => __( 'Hauptnavigation', 'dravet' ),
+		'footer-1' => __( 'Fusszeile 1', 'dravet' ),
+		'footer-2' => __( 'Fusszeile 2', 'dravet' ),
+		'footer-3' => __( 'Fusszeile 3', 'dravet' ),
+		'footer-4' => __( 'Fusszeile 4', 'dravet' ),
+		'footer-5' => __( 'Fusszeile 5', 'dravet' ),
+		'footer-6' => __( 'Fusszeile 6', 'dravet' ),
+		'footer-7' => __( 'Fusszeile 7', 'dravet' ),
+	) 
+);
 
-// This theme uses wp_nav_menu() in one location.
-if ( ! function_exists( 'register_nav_menus' ) ) {
-	
-	register_nav_menus( 
-		array(
-			'primary' => esc_html__( 'Hauptnavigation', 'dravet' ),
-			'footer-1' => esc_html__( 'Fusszeile 1', 'dravet' ),
-			'footer-2' => esc_html__( 'Fusszeile 2', 'dravet' ),
-			'footer-3' => esc_html__( 'Fusszeile 3', 'dravet' ),
-			'footer-4' => esc_html__( 'Fusszeile 4', 'dravet' ),
-			'footer-5' => esc_html__( 'Fusszeile 5', 'dravet' ),
-			'footer-6' => esc_html__( 'Fusszeile 6', 'dravet' ),
-			'footer-7' => esc_html__( 'Fusszeile 7', 'dravet' ),
-		) 
-	);
-}
 
 // dravet navigation
 if ( ! function_exists( 'bulmastarter_navigation' ) ) {
